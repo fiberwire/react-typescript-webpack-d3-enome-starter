404425526b069f3eb8fa90f38fdcd08bdf918b09
fiberwire <andrew.ramos92@gmail.com>
2017-06-15 16:38:34 -0400

    initial commit

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -10,9 +10,7 @@ export interface AppProps {
 export default class App extends React.Component<AppProps, undefined> {
     render() {
         return <div className="app">
-            <h1>Hello World!</h1>
-            <p>Foo to the barz</p>
-            <img src={reactLogo}/>
+
         </div>;
     }
 }
