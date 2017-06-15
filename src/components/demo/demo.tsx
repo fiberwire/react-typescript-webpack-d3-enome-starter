d8603c4e8a509da8e04320ea3b757fd1f933b37f
fiberwire <andrew.ramos92@gmail.com>
2017-06-15 19:50:13 -0400

    initialized pop, passed data to drawing

--- src/components/demo/demo.tsx
+++ src/components/demo/demo.tsx
@@ -4,6 +4,8 @@ import * as React from "React";
 import { Flex, Box } from "reflexbox";
 import { DemoStats } from "../demo-stats/demo-stats";
 import { DemoControls } from "../demo-controls/demo-controls";
+import { Population } from "../../population";
+import { Point } from "../../point";
 
 interface Props {
     drawingWidth: number;
@@ -11,20 +13,24 @@ interface Props {
 }
 
 interface State {
-
+    data: Point[];
 }
 
 export class Demo extends React.Component<Props, State> {
+    pop: Population;
+
     constructor(props: Props) {
         super(props);
+        this.pop = new Population(this.props.drawingWidth, this.props.drawingHeight, 50);
     }
 
     render() {
         return <Flex align="center" className="demo">
             <Box p={4} w={8}>
                 <Drawing
-                width={this.props.drawingWidth}
-                height={this.props.drawingHeight}
+                    width={this.props.drawingWidth}
+                    height={this.props.drawingHeight}
+                    data={this.state.data}
                 />
             </Box>
 
