c69694d5ef664b5932e290bfae683303af55026f
fiberwire <andrew.ramos92@gmail.com>
2017-06-15 18:36:26 -0400

    added demo-controls and demo-stats components

---
+++ src/components/demo-controls/demo-controls.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+
+interface Props {
+
+}
+
+interface State {
+
+}
+
+export class DemoControls extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+    }
+
+    render() {
+        return <div>
+
+        </div>;
+    }
+}
---
+++ src/components/demo-stats/demo-stats.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+
+interface Props { }
+
+interface State { }
+
+export class DemoStats extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+    }
+
+    render() {
+        return <div>
+
+        </div>;
+    }
+}
