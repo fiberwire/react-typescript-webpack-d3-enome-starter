6f4e5c0b8aca31694244f20d876eceabe908cef7
fiberwire <andrew.ramos92@gmail.com>
2017-06-17 00:27:22 -0400

    removed HMR stuff

--- src/index.tsx
+++ src/index.tsx
@@ -5,25 +5,4 @@ import App from "./components/App";
 
 const rootEl = document.getElementById("root");
 
-render(
-    <AppContainer>
-        <App/>
-    </AppContainer>,
-    rootEl
-);
-
-// Hot Module Replacement API
-declare let module: {hot: any};
-
-if (module.hot) {
-    module.hot.accept("./components/App", () => {
-        const NewApp = require("./components/App").default;
-
-        render(
-            <AppContainer>
-                <NewApp/>
-            </AppContainer>,
-            rootEl
-        );
-    });
-}
+render(<App/>, rootEl);
