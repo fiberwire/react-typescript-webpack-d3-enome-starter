b24bae9b2629344897e49e21a9612f621802ed1b
fiberwire <andrew.ramos92@gmail.com>
2017-08-11 10:38:09 -0400

    added circle simulation set up

--- src/components/demo/demo.tsx
+++ src/components/demo/demo.tsx
@@ -1,16 +1,24 @@
+import { ICircleOrgOptions } from "../../simulation/options/circle-org-options";
+import { ICirclePopOptions } from "../../simulation/options/circle-pop-options";
+import { ICircleGenomeOptions } from "../../simulation/options/circle-genome-options";
+import { ICircleEnvOptions } from "../../simulation/options/circle-env-options";
+import { CircleEnv } from "../../simulation/circle-env";
+import { CirclePop } from "../../simulation/circle-pop";
+import { CircleSim } from "../../simulation/circle-sim";
 import { Drawing } from "../drawing/drawing";
 
 import * as React from "React";
-import { Flex, Box } from "reflexbox";
 import { DemoStats } from "../demo-stats/demo-stats";
 import { DemoControls } from "../demo-controls/demo-controls";
-import { Population } from "../../population";
-import { BehaviorSubject, IDisposable } from "rx";
 import { Row, Col } from "react-flexbox-grid";
 
 import * as _ from "lodash";
 import { Point } from "../../interfaces/point";
 import { HistoryState } from "../../interfaces/history-state";
+
+import { GenomeRefill, FitnessObjective, MutateOp, UpdateType } from "enome";
+import { Subscription } from "rxjs/Subscription";
+import { BehaviorSubject } from "rxjs/BehaviorSubject";
 
 interface Props {
     drawingWidth: number;
@@ -27,12 +35,13 @@ interface State {
 }
 
 export class Demo extends React.Component<Props, State> {
-    pop: Population;
-    evolution: IDisposable;
+    evolution: Subscription;
+    sim: CircleSim;
 
     constructor(props: Props) {
         super(props);
-        this.pop = new Population(this.props.drawingWidth, this.props.drawingHeight, 50);
+
+        // initialize state
         this.state = {
             data: [],
             fitness: 0,
@@ -41,6 +50,58 @@ export class Demo extends React.Component<Props, State> {
             history: [],
             generation: 0
         };
+
+        const genOptions: ICircleGenomeOptions = {
+            genomeLength: 100,
+            geneLength: 1,
+            circles: 50,
+            minX: 0,
+            maxX: this.props.drawingWidth,
+            minY: 0,
+            maxY: this.props.drawingHeight,
+            radius: 5,
+            margin: 35,
+            refill: GenomeRefill.extend,
+        };
+
+        const popOptions: ICirclePopOptions = {
+            generations: 1000,
+            mutate: {
+                mutateChance: 0.05,
+                mutateOp: MutateOp.sub,
+            },
+            objective: FitnessObjective.maximize,
+            progress: true,
+            size: 10,
+            topPercent: .25,
+            weights: {
+                keep: 5,
+                mutate: 15,
+                randomize: 10,
+                reproduce: 70,
+            },
+        };
+
+        const orgOptions: ICircleOrgOptions = {
+            interactions: 1,
+            radius: 35,
+        };
+
+        const envOptions: ICircleEnvOptions = {
+            interactionRate: 1000,
+            updateType: UpdateType.random,
+        };
+
+        // initialize sim
+        let pop = new CirclePop(
+            genOptions,
+            popOptions,
+            orgOptions
+        );
+
+        let env = new CircleEnv(envOptions);
+
+        this.sim = new CircleSim(pop, env);
 
         this.state.evolving
             .subscribe(e => {
@@ -62,26 +123,26 @@ export class Demo extends React.Component<Props, State> {
             fitness: 0
         });
 
-        this.state.reset.onNext(false);
+        this.state.reset.next(false);
     }
 
     startEvolution = () => {
         if (this.evolution != null) {
-            this.evolution.dispose();
+            this.evolution.unsubscribe();
             this.evolution = null;
         }
 
         console.log("beginning evolution");
 
-        this.evolution = this.pop.evolve$(15, 500)
+        this.evolution = this.sim.start().best
             .subscribe(e => {
-                console.log(e.genome.id);
+                console.log(e.genotype.id);
                 this.setState({
-                    data: e.result,
+                    data: e.phenotype,
                     fitness: e.fitness,
                     history: _.concat(
                         this.state.history,
-                        { data: e.result, fitness: e.fitness, generation: this.state.generation, genomes: this.pop.genomes }
+                        { data: e.phenotype, fitness: e.fitness, generation: this.state.generation, genomes:  }
                     ),
                     generation: this.state.generation + 1
                 });
@@ -90,7 +151,7 @@ export class Demo extends React.Component<Props, State> {
 
     stopEvolution = () => {
         if (this.evolution != null) {
-            this.evolution.dispose();
+            this.evolution.unsubscribe();
             this.evolution = null;
         }
     }
