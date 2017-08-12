6de49312c4d0fac0e9f181c7036d4f6ac5cb7904
fiberwire <andrew.ramos92@gmail.com>
2017-08-12 10:09:25 -0400

    fixed imports

--- src/simulation/circle-env.ts
+++ src/simulation/circle-env.ts
@@ -1,6 +1,6 @@
-import { ICircleGenomeOptions } from "./circle-genome-options";
-import { ICirclePopOptions } from "./circle-pop-options";
-import { ICircleOrgOptions } from "./circle-org-options";
+import {ICircleOrgOptions} from "./options/circle-org-options";
+import {ICirclePopOptions} from "./options/circle-pop-options";
+import {ICircleGenomeOptions} from "./options/circle-genome-options";
 import { ICircleData } from "./circle-data";
 import { Point } from "../interfaces/point";
 import { ICircleState } from "./circle-state";
--- src/simulation/circle-sim.ts
+++ src/simulation/circle-sim.ts
@@ -1,10 +1,10 @@
 import { ICircleData } from "./circle-data";
 import { Point } from "../interfaces/point";
-import { ICircleOrgOptions } from "./circle-org-options";
-import { ICirclePopOptions } from "./circle-pop-options";
-import { ICircleGenomeOptions } from "./circle-genome-options";
 import { ICircleState } from "./circle-state";
 import { Simulation } from "enome";
+import { ICircleGenomeOptions } from "./options/circle-genome-options";
+import { ICirclePopOptions } from "./options/circle-pop-options";
+import { ICircleOrgOptions } from "./options/circle-org-options";
 
 export class CircleSim extends Simulation<
 ICircleGenomeOptions,
