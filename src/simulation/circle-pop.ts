0c0a2b913a3c7430ebfecda56c193fd7ef6dafa3
fiberwire <andrew.ramos92@gmail.com>
2017-08-12 10:11:31 -0400

    implemented CirclePop

--- src/simulation/circle-pop.ts
+++ src/simulation/circle-pop.ts
@@ -1,11 +1,13 @@
+import {ICircleOrgOptions} from "./options/circle-org-options";
+import {ICirclePopOptions} from "./options/circle-pop-options";
+import {ICircleGenomeOptions} from "./options/circle-genome-options";
 import { Population, Genome, IEvaluation } from "enome";
 import { ICircleState } from "./circle-state";
 import {Point} from "../interfaces/point";
 import { ICircleData } from "./circle-data";
-import { ICircleOrgOptions } from "./circle-org-options";
-import { ICirclePopOptions } from "./circle-pop-options";
-import { ICircleGenomeOptions } from "./circle-genome-options";
 import { CircleOrg } from "./circle-org";
+
+import * as _ from "lodash";
 
 export class CirclePop extends Population<
 ICircleGenomeOptions,
@@ -13,12 +15,9 @@ ICirclePopOptions,
 ICircleOrgOptions,
 ICircleData, Point[], ICircleState, ICircleState> {
 
-    mutate(evaluation: IEvaluation<ICircleGenomeOptions, ICircleData, Point[]>): Genome<ICircleGenomeOptions> {
-        throw new Error("Method not implemented.");
-    }
 
     createOrganism(genome: Genome<ICircleGenomeOptions>, options: ICircleOrgOptions): CircleOrg {
-        throw new Error("Method not implemented.");
+        return new CircleOrg(new Genome<ICircleGenomeOptions>(this.genOptions), this.orgOptions);
     }
 
 }
